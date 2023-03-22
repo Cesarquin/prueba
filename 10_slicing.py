5d5c997ee3fa0e5ea1df09e770d8d1b33e356461
Cesar Quintero <104667467+Cesarquin@users.noreply.github.com>
2023-03-22 07:00:00 -0500

    Versión 1.
    Estos archivos explican de manera inicial, el uso de python.

---
+++ 10_slicing.py
@@ -0,0 +1,18 @@
+nombre = 'Cesar Quintero'
+# INDEXING:
+# Posición índice de 'C' es cero:
+print(nombre[0])
+print(nombre[4])
+# SLICING:
+print(nombre[1:4])
+print(nombre[2:7])
+print(nombre[7:12])
+print(nombre[0:14])
+print(nombre[0:])
+# CsrQ
+print(nombre[0:13])
+print(nombre[0:13:2])
+print(nombre[0:13:3])
+# Regresivo:
+print('Regresivo:')
+print(nombre[-1:-9:-1])
